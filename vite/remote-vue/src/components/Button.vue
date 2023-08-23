1c7fac2bc48b092385bb6927d07cbf4c0b500bac
naughtLdy <7417867+naughtLdy@users.noreply.github.com>
2023-08-23 19:24:50 +0900

    feat: add remote-vue app in vite

---
+++ vite/remote-vue/src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="button" type="button" @click="count++">
+    count is {{ count }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<style scoped>
+.button {
+  background-color: #41b883;
+}
+</style>
---
+++ vite/remote-vue/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    federation({
+      name: "remote_vue",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/components/Button.vue",
+      },
+      shared: require("./package.json").dependencies,
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
