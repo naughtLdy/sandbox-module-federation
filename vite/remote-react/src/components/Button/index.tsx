377b03c9ce4a3c1df6644844eb825c687bd6c285
naughtLdy <7417867+naughtLdy@users.noreply.github.com>
2023-08-23 18:41:34 +0900

    feat: add remote-react app in vite

---
+++ vite/remote-react/src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./index.css";
+
+const Button = () => {
+  const [count, setCount] = useState(0);
+
+  return <button onClick={() => setCount(count + 1)}>count is {count}</button>;
+};
+
+export default Button;
---
+++ vite/remote-react/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "remote_react",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/components/Button",
+      },
+      shared: require("./package.json").dependencies,
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
