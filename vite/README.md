33537c303ee4de0b1cb28d69481bbbdb09a2c6dd
naughtLdy <7417867+naughtLdy@users.noreply.github.com>
2023-08-23 19:01:00 +0900

    feat: add host-react app in vite

---
+++ vite/README.md
@@ -0,0 +1,17 @@
+## vite module federation
+
+### react
+
+run remote
+
+```sh
+cd remote-react
+npm run build && npm run serve
+```
+
+run host
+
+```sh
+cd host-react
+npm run dev
+```
--- vite/remote-react/src/components/Button/index.tsx
+++ vite/remote-react/src/components/Button/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 const Button = () => {
   const [count, setCount] = useState(0);
 
-  return <button onClick={() => setCount(count + 1)}>count is {count}</button>;
+  return <button className="button" onClick={() => setCount(count + 1)}>count is {count}</button>;
 };
 
 export default Button;
