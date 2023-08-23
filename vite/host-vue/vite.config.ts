73be284cb1572ef59b998d36bdd9d31b2edafa04
naughtLdy <7417867+naughtLdy@users.noreply.github.com>
2023-08-23 19:30:07 +0900

    feat: add host-vue app in vite

--- vite/host-vue/vite.config.ts
+++ vite/host-vue/vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import vue from "@vitejs/plugin-vue";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    vue(),
     federation({
-      name: "host_react",
+      name: "host_vue",
       remotes: {
-        remote_react: "http://localhost:5001/assets/remoteEntry.js",
+        remote_vue: "http://localhost:5002/assets/remoteEntry.js",
       },
       shared: require("./package.json").dependencies,
     }),
